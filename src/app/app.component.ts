1318b06737bc914c59210d8afd78d9786f523922
mikevive <mikevive327@gmail.com>
2022-08-29 13:29:29 -0500

    responsive added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sandia';
+export class AppComponent implements OnInit{
+  isLoading: boolean = true;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.isLoading = false;
+    }, 100)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './components/pages/public/splash/splash.component';
+import { MainComponent } from './components/pages/public/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
